03283757ed432b92cb27c3d86a09ac6fb8c7d915
Silvia Preston <spreston@prcc.edu>
2024-01-09 09:19:22 -0600

    New clientside folder

---
+++ clientSide/Chapter01/project04/project01-04.js
@@ -0,0 +1,20 @@
+/*  JavaScript 7th Edition
+    Chapter 1
+    Hands-On Project 1-4
+
+    Author: 
+    Date:   
+
+    Filename: project01-04.js
+*/
+
+//define variables for home and work addresses
+homeStreet = "1 Main St.";
+homeCity = "Sicilia";
+homeState = "MA";
+homeCode = "02103";
+
+workStreet = "15 Oak Ln.";
+workCity = "Central City";
+workState = "MA";
+workCode = "02104";
