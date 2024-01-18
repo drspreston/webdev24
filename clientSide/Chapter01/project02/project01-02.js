7e500bda8f26cc752e109d3e9ae5c2faa678fd96
Silvia Preston <spreston@prcc.edu>
2024-01-18 12:25:09 -0600

    Client-Side Chapter 1 Hands-on projects 1 and 2

---
+++ clientSide/Chapter01/project02/project01-02.js
@@ -0,0 +1,20 @@
+/*  JavaScript 7th Edition
+    Chapter 1
+    Hands-On Project 1-2
+
+    Author: 
+    Date:   
+
+    Filename: project01-02.js
+*/
+
+//define variables for service name and service speed
+let service1Name = "Basic";
+let service2Name = "Express";
+let service3Name = "Extreme";
+let service4Name = "Ultimate";
+
+let service1Speed = "50 Mbsp";
+let service2Speed = "100 Mbsp";
+let service3Speed = "500 Mbsp";
+let service4Speed = "1 Gig";
