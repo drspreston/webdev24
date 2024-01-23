cdda4a0ddeba08a50ecee4d9e540ecc3c26bfb0b
Silvia Preston <spreston@prcc.edu>
2024-01-23 09:16:39 -0600

    remove handson project 1-3

--- clientSide/Chapter01/project04/project01-04.js
+++ clientSide/Chapter01/project04/project01-04.js
@@ -9,12 +9,3 @@
 */
 
 //define variables for home and work addresses
-homeStreet = "1 Main St.";
-homeCity = "Sicilia";
-homeState = "MA";
-homeCode = "02103";
-
-workStreet = "15 Oak Ln.";
-workCity = "Central City";
-workState = "MA";
-workCode = "02104";
