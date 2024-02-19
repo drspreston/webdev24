fae2151ac5f8f9b02030fc524bc757d2048ce0aa
Silvia Preston <spreston@prcc.edu>
2024-02-19 11:44:52 -0600

    fitnessclub updates and webdev homepage updates

--- clientSide/Chapter02/project01/project02-01_txt.js
+++ clientSide/Chapter02/project01/project02-01_txt.js
@@ -9,3 +9,23 @@
       Filename: project02-01.js
  */
 
+function CelsiusToFahrenheit(degree)
+{
+      return (degree*1.8) + 32;  
+}
+
+function FahrenheitToCelsius(degree)
+{
+      return (degree-32)/1.8;
+}
+
+
+document.getElementById("cValue").onchange = function() {
+      let cDegree = document.getElementById("cValue").value;
+      document.getElementById("fValue").value = CelsiusToFahrenheit(cDegree);
+};
+
+document.getElementById("fValue").onchange = function() {
+      let fDegree = document.getElementById("fValue").value;
+      document.getElementById("cValue").value = FahrenheitToCelsius(fDegree);
+};
